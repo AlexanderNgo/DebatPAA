024888ee46cc32a7ae7b605d42b5a9fd2fc01948
AlexanderNgo <117934442+AlexanderNgo@users.noreply.github.com>
2022-11-11 14:32:49 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main 
+{
+	public static void main(String[] args) 
+	{
+		Ensemble e = new Ensemble();
+		e.remplissageArguments();
+		e.menu();
+		e.afficherArguments();
+		e.menu2();
+	}
+}
