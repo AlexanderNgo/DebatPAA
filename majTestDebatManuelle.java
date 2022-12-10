350a1348e4601b1b73fc6bf5c4c9fed28b96a3f4
AlexanderNgo <117934442+AlexanderNgo@users.noreply.github.com>
2022-12-10 23:09:14 +0100

    Update majTestDebatManuelle.java

--- majTestDebatManuelle.java
+++ majTestDebatManuelle.java
@@ -2,39 +2,31 @@ package programmeManuelle;
 
 import interfaceUtilisateur.Menu;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class TestDebatManuelle {
 	
-	public static void main(String[] args) {
-		try
+	public static void main(String[] args) 
+	{
+		Menu m = new Menu();                        // Classe pour afficher les diff�rentes parties du menu
+		DebatManuelle debat =new DebatManuelle();  // Pour cr�er le d�bat � partir du fichier donn� en param�tre de menuAutoFichier
+		Scanner scan = new Scanner(System.in);
+		int res;
+		//System.out.println("1) remplir le graphe manuellement 2) automatiquement ? : \n");
+		res = m.saisi("1) remplir le graphe manuellement 2) automatiquement ? : \n");
+		while(res != 1 && res !=2)
 		{
-			Menu m = new Menu();                        // Classe pour afficher les diff�rentes parties du menu
-			DebatManuelle debat =new DebatManuelle();  // Pour cr�er le d�bat � partir du fichier donn� en param�tre de menuAutoFichier
-			Scanner scan = new Scanner(System.in);
-			int res;
-			System.out.println("1) remplir le graphe manuellement 2) automatiquement ? : \n");
-			res = scan.nextInt();
-			while(res != 1 && res !=2)
-			{
-				System.out.println("Entrez soit 1 soit 2 : \n");
-				res = scan.nextInt();
-			}
-			if(res == 1)
-				m.menuManuelle(debat);
-			else if(res == 2)
-				m.menuAutoFichier(args[0], debat);
-			//m.menuMannelleSol(debat);
-			m.menuAutoSolu(debat);
-			scan.close();
+			//System.out.println("Entrez soit 1 soit 2 : \n");
+			res = m.saisi("Entrez soit 1 soit 2 : \n");
 		}
-		catch(InputMismatchException e)
-		{
-			e = new InputMismatchException("Erreur, veuillez entrer un nombre entier lors de la saisie");
-			System.out.println(e.getMessage());
-		}
+		if(res == 1)
+			m.menuManuelle(debat);
+		else if(res == 2)
+			m.menuAutoFichier(args[0], debat);
+		//m.menuMannelleSol(debat);
+		m.menuAutoSolu(debat);
+		scan.close();
 	}	
 		
 }
